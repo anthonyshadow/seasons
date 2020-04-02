4ad213e67d0649aa27d2fec2a4287ff43f993ed4
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2020-04-02 18:14:17 -0400

    added conditional rendering for if user accepts or denys location

--- src/index.js
+++ src/index.js
@@ -8,26 +8,47 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {lat: null };
+    this.state = {lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         //sets latitude in state
         this.setState({lat: position.coords.latitude})
       },
-      (err) => console.log(err)
+      (err) => {
+        //sets error message
+        // console.log("error", err)
+        this.setState({errorMessage: err.message})
+      }
     );
   }
   
   render() {
+      
+      if (this.state.errorMessage && !this.state.lat) {
+        return (
+          <div>
+            Error: {this.state.errorMessage}
+          </div>
+        )
+      };
 
+      if (this.state.lat && !this.state.errorMessage) {
+        return (
+          <div>
+            Latitude : {this.state.lat}
+          </div>
+        )
+      };
 
+      return (
+        <div>
+          Please wait your webpage is being loaded
+        </div>
+      )
 
-    return (
-      <div>Latitude: {this.state.lat} </div>
-    )
-  }
-}
+  };
+};
 
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
